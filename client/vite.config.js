5579ae04efb0b18da67e852055ee9db1e61bf4be
taehunt <wxg005@naver.com>
2025-04-13 20:03:00 +0900

    ver 02

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/', // 👉 꼭 명시해야 루트로 접근 가능
 })
