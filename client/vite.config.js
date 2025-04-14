3b9fa98a9e4f8ffcb585b8d3ced7604ec06f5ea5
taehunt <wxg005@naver.com>
2025-04-15 06:36:29 +0900

    환경변수 자동 분리 적용 (.env.production / vite.config.js 개선)

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,16 +1,27 @@
-import { defineConfig } from 'vite'
+// client/vite.config.js
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
-export default defineConfig({
-  plugins: [react()],
-  base: '/',
-  build: {
-	rollupOptions: {
-	  output: {
-		entryFileNames: `assets/[name].[hash].js`,
-		chunkFileNames: `assets/[name].[hash].js`,
-		assetFileNames: `assets/[name].[hash].[ext]`,
-	  },
-	},
-  },
+export default defineConfig(({ mode }) => {
+  loadEnv(mode, process.cwd()) // 환경 변수는 로딩만 해주면 됨
+
+  return {
+    plugins: [react()],
+    base: '/',
+    build: {
+      rollupOptions: {
+        output: {
+          entryFileNames: `assets/[name].[hash].js`,
+          chunkFileNames: `assets/[name].[hash].js`,
+          assetFileNames: `assets/[name].[hash].[ext]`,
+        },
+      },
+    },
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, 'src'),
+      },
+    },
+  }
 })
