e1310e95140da1aac9ba4dafb51074157c296db7
taehunt <wxg005@naver.com>
2025-04-17 04:56:48 +0900

    20250417 Client 폴더 구조 수정

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -68,7 +68,7 @@ function Navbar() {
             <div className="hover:text-blue-500 navMenu block py-2 cursor-pointer">
               전자책
             </div>
-            <div className="absolute left-0 mt-2 w-48 bg-white border rounded shadow-lg z-10 opacity-0 invisible group-hover:visible group-hover:opacity-100 transition-opacity duration-200">
+            <div className="absolute left-0 mt-0 w-48 bg-white border rounded shadow-lg z-10 opacity-0 invisible group-hover:visible group-hover:opacity-100 transition-opacity duration-200">
               <Link
                 to="/books?category=planning"
                 className="block px-4 py-2 text-sm hover:bg-gray-100"
