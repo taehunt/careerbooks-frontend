39800a901f9d9f40b3e240c27d7c20da4f42991f
taehunt <wxg005@naver.com>
2025-04-26 18:44:37 +0900

    20250426 Client Footer 수정

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -8,9 +8,6 @@ export default function Footer() {
       <div className="max-w-6xl mx-auto px-4 flex flex-col sm:flex-row justify-between items-center space-y-2 sm:space-y-0">
         <div>&copy; {new Date().getFullYear()} CareerBooks. All rights reserved.</div>
         <div className="flex space-x-4">
-          <Link to="/" className="hover:underline">
-            홈
-          </Link>
           <span className="hover:underline">
             예금주 : 장태훈  |  은행명 : 기업은행  |  계좌번호 : 688-014027-01-011
           </span>
